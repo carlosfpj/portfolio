35becfbae736c3903bc3242923290ef4d43ac24c
carlosfpj <cafer89@gmail.com>
2022-05-29 21:48:14 -0500

    click behavior test 2

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-
 var $menuButton = document.querySelector('.menuButton');
 var $navbar = document.querySelector('.nav')
-console.log($menuButton);
-console.log($navbar);
 $menuButton.onpointerdown = downHandler;
-
-
-
 
 function downHandler (event) {
   console.log("evento de click");
   $navbar.classList.toggle('active');
 }
-
-// $menuButton.addEventListener('Pointer' , downHandler)
