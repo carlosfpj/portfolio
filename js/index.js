ef63ebac85e3f7353314f8309f890f8c6f68b485
carlosfpj <cafer89@gmail.com>
2022-07-04 15:20:09 -0500

    accordion test

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 var $menuButton = document.querySelector('.menuButton');
 var $navbar = document.querySelector('.nav')
 $menuButton.onpointerdown = downHandler;
+let bloque;
+
 
 function downHandler (event) {
   console.log("evento de click");
   $navbar.classList.toggle('inactive');
 }
+
+
+const h2 = document.querySelectorAll('.h2');
+
+h2.forEach((cadah2, i) => {
+  h2[i].addEventListener('click', ()=> {
+    bloque = cadah2.parentElement;
+    bloque.classList.toggle('activo');
+  })
+})
