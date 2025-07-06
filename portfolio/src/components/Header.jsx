7bea91c0f0416e7e3f049f7bf728d658e77ea188
carlosfpj <cafer89@gmail.com>
2025-07-06 14:56:08 -0500

    add color to hero span

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -17,8 +17,8 @@ const Header = ({show}) => {
         <p className="greeting">HI THERE!!! I AM</p>
         <h1 className="name">Carlos Fernando</h1>
         <p className="smallBio">
-          <span>Chemical Engineer</span> with 8 years of experience in the
-          Oil&amp;Gas industry and <span>enthusiastic about programming</span> and
+          <span className="oil-gas-color1">Chemical Engineer</span> with 8 years of experience in the
+          Oil&amp;Gas industry and <span className="programming-color1">enthusiastic about programming</span> and
           experienced in either energy or software-related fields.
         </p>
         <div className="hero-socials">
@@ -26,16 +26,28 @@ const Header = ({show}) => {
           <ul className={`hero-socials-list `}>
             <li>
               <a href="https://www.linkedin.com/in/carlosfpj/" target="_blank" rel="noopener noreferrer">
-                <IconContext.Provider value={{ className: "icon-linkedin" }}>
+                {!show ?
+                  <IconContext.Provider value={{ className: "icon-linkedin", color: "#8444df" }}>
                   <ImLinkedin />
                 </IconContext.Provider>
+                :
+                  <IconContext.Provider value={{ className: "icon-linkedin", color: "#7D5260" }}>
+                    <ImLinkedin />
+                  </IconContext.Provider>
+                }
               </a>
             </li>
             <li>
               <a href="https://github.com/carlosfpj" target="_blank" rel="noopener noreferrer">
-                <IconContext.Provider value={{ className: "icon-linkedin" }}>
-                  <FaGithub />
-                </IconContext.Provider>
+                {!show ?
+                  <IconContext.Provider value={{ className: "icon-linkedin", color: "#8444df"}}>
+                    <FaGithub />
+                  </IconContext.Provider>
+                :
+                  <IconContext.Provider value={{ className: "icon-linkedin", color: "#7D5260" }}>
+                    <FaGithub />
+                  </IconContext.Provider>
+                }
               </a>
             </li>
           </ul>
